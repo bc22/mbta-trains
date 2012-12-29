55df399d840d27fe83b0d2a06529588031344fa2
Adam Lamarre <aelamarre@erzr-macbook.local>
2012-12-29 17:00:10 -0500

    views

--- js/app/app.js
+++ js/app/app.js
@@ -1,4 +1,15 @@
+Backbone.View.prototype.close = function() {
+    this.unbind();
+    this.remove();
+
+    if (this.onClosing) {
+        this.onClosing();
+    }
+};
+
 var app = app || {};
+
+app.events = app.events || _.extend({}, Backbone.Events);
 
 (function(models){
 
@@ -46,16 +57,157 @@ var app = app || {};
 
     collections.Platforms = Backbone.Collection.extend({
         model: app.models.Platform,
-        url: 'data/Platforms.json'
+        url: 'data/Platforms.json',
+        groupedByStation: function() {
+            return this.groupBy(function(model) {
+                return model.get('StationName');
+            });
+        },
+        getPlatformsByStation: function(station){
+            return this.filter(function(model) {
+                var v = model.get('StationName');
+                return v.toLowerCase() === station.toLowerCase();
+            });
+        }
     });
 
 })(app.collections = app.collections || {});
 
-$(function() {
-    var c = new app.collections.Platforms();
-    c.fetch({
-        success: function() {
-            alert(c.models.length);
+(function(views){
+
+    views.BaseListView = Backbone.View.extend({
+        tagName: 'ul',
+        className: 'list inset'
+    });
+
+    views.CurrentStopListItem = Backbone.View.extend({
+        tagName: 'li',
+        events: {
+            'click': function() {
+                var url = 'platform/' + this.model.toLowerCase();
+                app.router.navigate(url, true);
+                return false;
+            }
+        },
+        render: function() {
+            this.$el.html(this.model);
+            return this;
         }
     });
+
+    views.CurrentStopList = views.BaseListView.extend({
+        render: function() {
+            var groups = this.collection.groupedByStation();
+
+            _(groups).each(function(platforms, key){
+                var v = new app.views.CurrentStopListItem({model:key, collection: this.collection});
+                this.$el.append(v.render().el);
+            }, this);
+
+            return this;
+        }
+    });
+
+    views.CurrentStopView = Backbone.View.extend({
+        initialize: function() {
+            this.collection = new app.collections.Platforms();
+            this.collection.fetch({async:false});
+        },
+        render: function() {
+            var v = new app.views.CurrentStopList({
+                collection: this.collection
+            });
+            this.$el.html(v.render().el);
+            return this;
+        }
+    });
+
+    views.PlatformListItem = Backbone.View.extend({
+        tagName: 'li',
+        events: {
+            'click': function() {
+                var station = this.model.get('StationName').toLowerCase();
+                var direction = this.model.get('Direction').toLowerCase();
+                var url = 'platform/' + station + '/' + direction;
+                app.router.navigate(url, true);
+            }
+        },
+        render: function() {
+            var direction = this.model.get('Direction');
+
+            if (direction === 'NB') {
+                this.$el.html('South Bound');
+            } else if (direction === 'SB') {
+                this.$el.html('North Bound');
+            }
+
+            return this;
+        }
+    });
+
+    views.PlatformList = views.BaseListView.extend({
+        render: function() {
+            var platforms = this.collection.getPlatformsByStation(this.model);
+
+            _(platforms).each(function(platform){
+                var v = new views.PlatformListItem({model:platform});
+                this.$el.append(v.render().el);
+            }, this);
+
+            return this;
+        }
+    });
+
+    views.PlatformListView = Backbone.View.extend({
+        initialize: function() {
+            this.collection = new app.collections.Platforms();
+            this.collection.fetch({async:false});
+        },
+        render: function() {
+            var v = new views.PlatformList({
+                collection: this.collection,
+                model: this.model
+            });
+
+            this.$el.html(v.render().el);
+
+            return this;
+        }
+    });
+
+})(app.views = app.views || {});
+
+(function(routers){
+
+    routers.Router = Backbone.Router.extend({
+       routes: {
+           '': 'index',
+           'platform/:name/:direction': 'direction',
+           'platform/:name': 'platform'
+       },
+       showView: function(view) {
+            if (this.currentView){
+                this.currentView.close();
+            }
+
+            this.currentView = view;
+            $('#content').html(this.currentView.render().el);
+            return view;
+        },
+        index: function() {
+            this.showView(new app.views.CurrentStopView());
+        },
+        platform: function(name) {
+            this.showView(new app.views.PlatformListView({model:name}));
+        },
+        direction: function(name, direction) {
+            
+        }
+    });
+
+})(app.routers = app.routers || {});
+
+$(function() {
+    app.router = new app.routers.Router();
+    Backbone.history.start();
 });
