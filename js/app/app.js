a074988d9606ebe1834be7ee9f57d85a921ec22e
Adam Lamarre <aelamarre@erzr-macbook.local>
2012-12-29 15:00:42 -0500

    stop activities

--- js/app/app.js
+++ js/app/app.js
@@ -1,7 +1,36 @@
-/**
- * Created with PyCharm.
- * User: aelamarre
- * Date: 12/29/12
- * Time: 2:44 PM
- * To change this template use File | Settings | File Templates.
- */
+var app = app || {};
+
+(function(models){
+
+    models.StopActivity = Backbone.Model.extend({
+        defaults: {
+            Line: null,
+            Trip: null,
+            PlatformKey: null,
+            InformationType: null,
+            Time: null,
+            TimeRemaining: null,
+            Revenue: null,
+            Route: null
+        }
+    });
+
+})(app.models = app.models || {});
+
+(function(collections){
+
+    collections.StopActivities = Backbone.Collection.extend({
+        model: app.models.Stop,
+        url: 'data/Red.json'
+    });
+
+})(app.collections = app.collections || {});
+
+$(function() {
+    var c = new app.collections.StopActivities();
+    c.fetch({
+        success: function() {
+            
+        }
+    });
+});
