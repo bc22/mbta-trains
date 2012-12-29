a86223ba47178c6228b9b901e3b5d203fa246d93
Adam Lamarre <aelamarre@erzr-macbook.local>
2012-12-29 15:18:26 -0500

    Platform model and collection

--- js/app/app.js
+++ js/app/app.js
@@ -2,6 +2,26 @@ var app = app || {};
 
 (function(models){
 
+    models.Platform = Backbone.Model.extend({
+        defaults: {
+            Line: null,
+            PlatformKey: null,
+            PlatformName: null,
+            StationName: null,
+            PlatformOrder: null,
+            StartOfLine: null,
+            EndOfLine: null,
+            Branch: null,
+            Direction: null,
+            stop_id: null,
+            stop_code: null,
+            stop_name: null,
+            stop_desc: null,
+            stop_lat: null,
+            stop_lon: null
+        }
+    });
+
     models.StopActivity = Backbone.Model.extend({
         defaults: {
             Line: null,
@@ -20,17 +40,22 @@ var app = app || {};
 (function(collections){
 
     collections.StopActivities = Backbone.Collection.extend({
-        model: app.models.Stop,
+        model: app.models.StopActivity,
         url: 'data/Red.json'
+    });
+
+    collections.Platforms = Backbone.Collection.extend({
+        model: app.models.Platform,
+        url: 'data/Platforms.json'
     });
 
 })(app.collections = app.collections || {});
 
 $(function() {
-    var c = new app.collections.StopActivities();
+    var c = new app.collections.Platforms();
     c.fetch({
         success: function() {
-            
+            alert(c.models.length);
         }
     });
 });
