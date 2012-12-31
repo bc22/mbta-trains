7c4ff405954c755b53bb1d50c8a53195255f9829
Adam Lamarre <aelamarre@erzr-macbook.local>
2012-12-31 10:25:59 -0500

    implemented 2.0 feed

--- js/app/app.js
+++ js/app/app.js
@@ -13,34 +13,26 @@ var app = app || {};
 
     models.Platform = Backbone.Model.extend({
         defaults: {
-            Line: null,
-            PlatformKey: null,
-            PlatformName: null,
-            StationName: null,
-            PlatformOrder: null,
-            StartOfLine: null,
-            EndOfLine: null,
-            Branch: null,
-            Direction: null,
-            stop_id: null,
-            stop_code: null,
-            stop_name: null,
-            stop_desc: null,
-            stop_lat: null,
-            stop_lon: null
+            "StopID": null,
+            "Stop": null
         }
     });
 
     models.StopActivity = Backbone.Model.extend({
         defaults: {
+            CurrentTime: null,
             Line: null,
-            Trip: null,
-            PlatformKey: null,
-            InformationType: null,
-            Time: null,
-            TimeRemaining: null,
-            Revenue: null,
-            Route: null
+            TripID: null,
+            Destination: null,
+            StopID: null,
+            Stop: null,
+            SecondsAway: null,
+            PosTimestamp: null,
+            TrainNumber: null,
+            PosLatitude: null,
+            PosLongitude: null,
+            PosHeading: null,
+            Note: null
         }
     });
 
@@ -50,37 +42,26 @@ var app = app || {};
 
     collections.StopActivities = Backbone.Collection.extend({
         model: app.models.StopActivity,
-        url: 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20xml%20where%20url%3D\'http%3A%2F%2Fdeveloper.mbta.com%2FData%2FRed.xml\'&format=json&diagnostics=true',
+        url: 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20csv%20where%20url%3D%22' +
+            'http%3A%2F%2Fdeveloper.mbta.com%2Flib%2Frthr%2Fred.csv%22%20and%20columns%20%3D%20%22CurrentTime%2C'  +
+            'Line%2CTripID%2CDestination%2CStopID%2CStop%2CSecondsAway%2CPosTimestamp%2CTrainNumber%2CPosLatitude%' +
+            '2CPosLongitude%2CPosHeading%2CNote%22%20and%20Line%20!%3D%20%22Line%22&format=json',
         sync: function(method, model, options){
             options.timeout = 10000;
             options.dataType = 'jsonp';
             return Backbone.sync(method, model, options);
         },
         parse: function(response) {
-            return response.query.results.Root.Red;
+            return response.query.results.row;
         },
-        getByPlatformKey: function(platformKey) {
-            return this.filter(function(model) {
-                var v = model.get('PlatformKey');
-                return v.toLowerCase() === platformKey.toLowerCase();
-            });
+        comparator: function(item) {
+            return Number(item.get('SecondsAway'))
         }
     });
 
     collections.Platforms = Backbone.Collection.extend({
         model: app.models.Platform,
-        url: 'data/Platforms.json',
-        groupedByStation: function() {
-            return this.groupBy(function(model) {
-                return model.get('StationName');
-            });
-        },
-        getPlatformsByStation: function(station){
-            return this.filter(function(model) {
-                var v = model.get('StationName');
-                return v.toLowerCase() === station.toLowerCase();
-            });
-        }
+        url: 'data/Platforms.json'
     });
 
 })(app.collections = app.collections || {});
@@ -96,23 +77,21 @@ var app = app || {};
         tagName: 'li',
         events: {
             'click': function() {
-                var url = 'platform/' + this.model.toLowerCase();
+                var url = 'platform/' + slugify(this.model.get('Stop'));
                 app.router.navigate(url, true);
                 return false;
             }
         },
         render: function() {
-            this.$el.html(this.model);
+            this.$el.html(this.model.get('Stop'));
             return this;
         }
     });
 
     views.CurrentStopList = views.BaseListView.extend({
         render: function() {
-            var groups = this.collection.groupedByStation();
-
-            _(groups).each(function(platforms, key){
-                var v = new app.views.CurrentStopListItem({model:key, collection: this.collection});
+            _(this.collection.models).each(function(station){
+                var v = new app.views.CurrentStopListItem({model:station});
                 this.$el.append(v.render().el);
             }, this);
 
@@ -121,43 +100,54 @@ var app = app || {};
     });
 
     views.CurrentStopView = Backbone.View.extend({
+        listView: null,
         initialize: function() {
-            this.collection = app.data.PlatformData;
+            this.listView = new app.views.CurrentStopList({
+                collection: app.data.PlatformData
+            });
         },
         render: function() {
-            var v = new app.views.CurrentStopList({
-                collection: this.collection
-            });
-            this.$el.html(v.render().el);
+            this.$el.html(this.listView.render().el);
             return this;
+        },
+        onClosing: function() {
+            if (this.listView) {
+                this.listView.close();
+            }
         }
     });
 
     views.PlatformListView = Backbone.View.extend({
         views: [],
-        initialize: function(options) {
+        cleanUpViews: function() {
             var v;
             while ((v = this.views.pop())) {
                 v.close();
             }
+        },
+        initialize: function(options) {
+            this.cleanUpViews();
 
-            var p = app.data.PlatformData.getPlatformsByStation(options.name);
+            var activities = app.data.ActivityData;
 
-            var activity = app.data.ActivityData;
+            // get all of the activities belonging to this station
+            var data = _(activities.models).filter(function(activity){
+                var id = slugify(activity.get('Stop'));
+                return id === options.id && activity.get('Destination') !== activity.get('Stop');
+            });
 
-            _(p).each(function(platform){
-                var key = platform.get('PlatformKey');
+            // group them by destination
+            var grouped = _(data).groupBy(function(item) {
+                return item.get('Destination');
+            });
 
-                var items = activity.getByPlatformKey(key);
+            _(grouped).each(function(group, key){
+                var v = new app.views.ActivityList({
+                    model: key,
+                    activities: group
+                });
 
-                if (items.length) {
-                    var v = new app.views.ActivityList({
-                        model: platform,
-                        collection: activity
-                    });
-
-                    this.views.push(v);
-                }
+                this.views.push(v);
             }, this);
         },
         render: function() {
@@ -172,32 +162,24 @@ var app = app || {};
     views.ActivityListItem = Backbone.View.extend({
         tagName: 'li',
         render: function() {
-            var m = moment(this.model.get('Time'));
+            var m = moment();
+            m.add('seconds', this.model.get('SecondsAway'));
             this.$el.html(m.fromNow());
             return this;
         }
     });
 
     views.ActivityList = views.BaseListView.extend({
+        initialize: function(options){
+            this.activities = options.activities;
+        },
         render: function() {
             this.$el.html('');
 
-            var direction = this.model.get('Direction');
-
-            if (direction === 'SB') {
-                direction = 'South Bound'
-            } else if (direction === 'NB') {
-                direction = 'North Bound';
-            }
-
-            var divider = $('<li class="list-divider">' + direction + '</li>');
+            var divider = $('<li class="list-divider">' + this.model + '</li>');
             this.$el.append(divider);
 
-            var key = this.model.get('PlatformKey');
-
-            var items = this.collection.getByPlatformKey(key);
-
-            _(items).each(function(item){
+            _(this.activities).each(function(item){
                 var v = new app.views.ActivityListItem({
                     model: item
                 });
@@ -216,7 +198,7 @@ var app = app || {};
     routers.Router = Backbone.Router.extend({
        routes: {
            '': 'index',
-           'platform/:name': 'platform'
+           'platform/:id': 'station'
        },
        showView: function(view) {
             if (this.currentView){
@@ -230,9 +212,9 @@ var app = app || {};
         index: function() {
             this.showView(new app.views.CurrentStopView());
         },
-        platform: function(name) {
+        station: function(id) {
             var v = new app.views.PlatformListView({
-                name: name
+                id: id
             });
             this.showView(v);
         }
